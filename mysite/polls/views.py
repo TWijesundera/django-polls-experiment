8409ee98f64dbd05fd6f4efb44eae1a0c3910d13
TWijesundera <thisarawijesundera@gmail.com>
2020-02-27 16:42:49 -0500

    modified polls up to HTTP404 error in tutorial

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,24 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
+from django.http import Http404
+
+from .models import Question
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {
+        'latest_question_list': latest_question_list
+    }
+    return render(request, 'polls/index.html', context)
+
+def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(f"You're looking at question {question_id}")
+
+def results(request, question_id):
+    return HttpResponse(f"You're looking at the result of question {question_id}")
+
+def vote(request, question_id):
+    return HttpResponse(f"You're voting on question {question_id}")
